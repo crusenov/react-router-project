116d9cdc2244896f37f30cfbd4a5fa1441e871cd
legion <tsvetan@airheadventures.com>
2020-01-24 16:40:23 +0200

    Routes - implementation

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './Home'
+import Players from './Players'
+import Teams from './Teams'
+import Navbar from './Navbar'
+import TeamPage from './TeamPage'
+import Articles from './Articles'
 
 function App() {
   return (
-    <div>
-     React Router course
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/players' component={Players} />
+          <Route path='/teams' component={Teams} />
+          <Route path='/:teamId' exact component={TeamPage} />
+          <Route path='/:teamId/articles' component={Articles} />
+          <Route render={() => <h1 className='text-center'>Four oh Four.</h1> }/>
+        </Switch>
+
+      </div>
+    </Router>
   )
 }
 
